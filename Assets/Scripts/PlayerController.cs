70ba132cfc6b2966d8005065e2c11c11612f5c23
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-12-04 17:56:20 +0300

    add local multiplayer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float _speed = 35.0f;
-    [SerializeField] private float _turnSpeed = 200.0f;
+    private float _speed = 35.0f;
+    private float _turnSpeed = 300.0f;
     private float _horizontalInput;
     private float _verticalInput;
+    public string inputID;
 
     void Update()
-    {
-        _horizontalInput = Input.GetAxis("Horizontal");
-        _verticalInput = Input.GetAxis("Vertical");
+    {   
+        _horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        _verticalInput = Input.GetAxis("Vertical" + inputID);
 
         transform.Translate(_speed * Time.deltaTime * _verticalInput * Vector3.forward);
         transform.Rotate(Vector3.up, _turnSpeed * Time.deltaTime * _horizontalInput);
-             
+
     }
 }
