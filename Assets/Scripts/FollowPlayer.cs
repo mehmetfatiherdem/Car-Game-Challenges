3cd386ce0a14b6b1def030d059bb5ca178318904
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-12-04 15:39:36 +0300

    add camera position switch

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
-    private Vector3 _offset = new (-0.6f, 6.96f, -10.02f);
+    private Vector3 _thirdPersonOffset = new (-0.6f, 6.96f, -10.02f);
+    private Vector3 _firstPersonOffset = new (0.0f, 3.0f, 0.0f);
+    private bool _cameraPos = true;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _cameraPos = !_cameraPos;
+        }        
+    }
 
     void LateUpdate()
-    {
-        transform.position = _player.transform.position + _offset;
+    {   
+
+        transform.position = _cameraPos ? _player.transform.position + _thirdPersonOffset : 
+            _player.transform.position + _firstPersonOffset;
     }
 }
